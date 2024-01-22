7b9308c24c4e117083d55414bb23a5b7503c3f9d
w0xid <157135992+w0xid@users.noreply.github.com>
2024-01-22 18:08:04 +0100

    Update Volvo240.java

--- Volvo240.java
+++ Volvo240.java
@@ -1,68 +1,15 @@
 import java.awt.*;
 
-public class Volvo240{
+public class Volvo240 extends CommonBaseCar {
 
-    public final static double trimFactor = 1.25;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
-    
-    public Volvo240(){
-        nrDoors = 4;
-        color = Color.black;
-        enginePower = 100;
-        modelName = "Volvo240";
-        stopEngine();
-    }
-    
-    public int getNrDoors(){
-        return nrDoors;
-    }
-    public double getEnginePower(){
-        return enginePower;
+    private final static double trimFactor = 1.25;
+
+    public Volvo240() {
+        super(4, Color.black, 100, "Volvo240");
     }
 
-    public double getCurrentSpeed(){
-        return currentSpeed;
-    }
-
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color clr){
-	    color = clr;
-    }
-
-    public void startEngine(){
-	    currentSpeed = 0.1;
-    }
-
-    public void stopEngine(){
-	    currentSpeed = 0;
-    }
-    
-    public double speedFactor(){
-        return enginePower * 0.01 * trimFactor;
-    }
-
-    public void incrementSpeed(double amount){
-	    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
-    }
-
-    public void decrementSpeed(double amount){
-        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
-    }
-
-    // TODO fix this method according to lab pm
-    public void gas(double amount){
-        incrementSpeed(amount);
-    }
-
-    // TODO fix this method according to lab pm
-    public void brake(double amount){
-        decrementSpeed(amount);
+    @Override
+    protected double speedFactor() {
+        return getEnginePower() * 0.01 * trimFactor;
     }
 }
