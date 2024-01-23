75bdefe36608bf3228c34607e17fec8566335f12
w0xid <157135992+w0xid@users.noreply.github.com>
2024-01-23 21:28:42 +0100

    Update Volvo240.java

--- Volvo240.java
+++ Volvo240.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 
-public class Volvo240 extends CommonBaseCar {
+public class Volvo240 extends CommonBaseCar implements Movable {
 
     private final static double trimFactor = 1.25;
 
@@ -11,5 +11,24 @@ public class Volvo240 extends CommonBaseCar {
     @Override
     protected double speedFactor() {
         return getEnginePower() * 0.01 * trimFactor;
+    }
+
+    @Override
+    public void move() {
+        double angleInRadians = Math.toRadians(getCurrentDirection());
+        double deltaX = getCurrentSpeed() * Math.cos(angleInRadians);
+        double deltaY = getCurrentSpeed() * Math.sin(angleInRadians);
+        setCurrentX(getCurrentX() + deltaX);
+        setCurrentY(getCurrentY() + deltaY);
+    }
+
+    @Override
+    public void turnLeft() {
+        setCurrentDirection((getCurrentDirection() - 90) % 360);
+    }
+
+    @Override
+    public void turnRight() {
+        setCurrentDirection((getCurrentDirection() + 90) % 360);
     }
 }
