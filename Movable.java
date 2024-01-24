289babfa294a47f13418d078207005d96b48a240
w0xid <157135992+w0xid@users.noreply.github.com>
2024-01-24 15:13:10 +0100

    Update Movable.java

--- Movable.java
+++ Movable.java
@@ -1,5 +1,11 @@
 public interface Movable {
-    void move();
-    void turnLeft();
-    void turnRight();
+    public void move() {
+        
+    }
+    public void turnLeft() {
+        
+    }
+    public void turnRight() {
+        
+    }
 }
